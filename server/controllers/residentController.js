7dcab354a42b8cabc6f0683f61f691487607c44e
Lina Shin <linashin@TLEs-MacBook-Pro.local>
2021-10-12 19:34:28 -0700

    added logout and updated qr

--- server/controllers/residentController.js
+++ server/controllers/residentController.js
@@ -5,7 +5,7 @@ const residentController = {};
 residentController.getResidentsAndGuests = async (req, res, next) => {
   try {
     const qResidentsAndGuests = {
-      text: 'SELECT * FROM users LEFT OUTER JOIN guests ON users._id=guests.resident_id WHERE users.admin=false'
+      text: 'SELECT users.username, users.address, guests._id, guests.first_name, guests.last_name, guests.email, guests.phone_number, guests.license_number, guests.resident_id FROM users LEFT OUTER JOIN guests ON users._id=guests.resident_id WHERE users.admin=false'
     }
     const qResidentsAndGuestsResults = await db.query(qResidentsAndGuests);
     res.locals.residentsAndGuests = qResidentsAndGuestsResults.rows;
